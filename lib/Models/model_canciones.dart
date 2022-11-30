89d762a4a6b349ad08bc7f0a565ad30ac52e4520
dorelysm <martinezdorelys@gmail.com>
2022-11-30 02:22:26 -0500

    first commit

---
+++ lib/Models/model_canciones.dart
@@ -0,0 +1,34 @@
+class Canciones {
+  List<ModelCanciones> items = [];
+
+  Canciones();
+
+  Canciones.fromJsonList(List<dynamic> jsonList){
+    if(jsonList == null) return;
+    for (var item in jsonList){
+      final cancion = new ModelCanciones.fromJsonMap(item);
+      items.add(cancion);
+    }
+  }
+}
+
+class ModelCanciones {
+  late String id;
+  late String titulo;
+  late String descripcion;
+  late String pdf;
+
+  ModelCanciones(
+    this.id,
+    this.titulo,
+    this.descripcion,
+    this.pdf,
+  );
+
+  ModelCanciones.fromJsonMap(Map<String, dynamic> json){
+    id = json['id'];
+    titulo = json['titulo'];
+    descripcion = json['descripcion'];
+    pdf = json['pdf'];
+  }
+}
