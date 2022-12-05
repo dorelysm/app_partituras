1b48e1c32c93081c4e34571f25fd38b8b8407fcd
dorelysm <martinezdorelys@gmail.com>
2022-12-05 18:08:16 -0500

    correcciones

--- lib/Pages/Cancion.dart
+++ lib/Pages/Cancion.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:pdf_viewer_flutter/pdf_viewer_flutter.dart';
-
+//import 'package:pdf_viewer_flutter/pdf_viewer_flutter.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:pdf_flutter/pdf_flutter.dart';
+//import 'package:advance_pdf_viewer/advance_pdf_viewer.dart';
 
 class Cancion extends StatefulWidget {
   const Cancion({super.key});
@@ -11,6 +14,17 @@ class Cancion extends StatefulWidget {
 }
 
 class _CancionState extends State<Cancion> {
+  bool _isLoading = true;
+  //late PDFDocument _pdf;
+  /*
+  void _loadFile() async {
+    _pdf = await PDFDocument.fromAsset('assets/Diraiton.pdf');
+
+    setState((){
+      _isLoading = false;
+    });
+  }
+  */
   @override
   void initState() {
     super.initState();
